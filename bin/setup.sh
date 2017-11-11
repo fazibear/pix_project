94a905ca68ac5a78566b0117cbb9fe8c614bac9a
Michał Kalbarczyk <fazibear@gmail.com>
2017-11-11 19:05:37 +0100

    Update setup script

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,4 +2,8 @@
 
 sudo apt-get install ruby-daemons raspberrypi-kernel-headers
 
+git clone git@github.com:fazibear/pix_kernel.git
+git clone git@github.com:fazibear/pix_ruby.git
+git clone git@github.com:fazibear/pix_elixir.git
+
 (cd pix_kernel && make)
